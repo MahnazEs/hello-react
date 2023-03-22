5cd6470dca0b3badad703f571fb733c793b4934f
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-03-22 13:10:05 -0700

    added

--- src/index.js
+++ src/index.js
@@ -3,24 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
-  // your code here
+  return (
+    <button onClick={ props.reset }>{props.children}</button>
+  );
 };
 
 const Application = () => {
+  const [name , setName] = useState("");
 
-  // your code here
+const reset = () => {
+    setName('');
+    console.log("name has been reset");
+};
 
-  const reset = () => {
-    console.log("reset");
-    // your code here
-  };
-
-  return (
-    <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
-    </main>
-  );
+return (
+  <main>
+    <input value={ name } onChange={(event) => setName(event.target.value)} placeholder= "Type your name"></input>
+    <Button reset={reset}>Reset</Button>
+    {name && <h1>Hello { name }</h1>}
+  </main>
+);
 };
 
 ReactDOM.render(<Application />, document.getElementById("root"));
